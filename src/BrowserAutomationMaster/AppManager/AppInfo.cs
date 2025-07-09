d3083be6357311d7ee13e69097762008ee38d295
STC-Codes <210101922+Static-Codes@users.noreply.github.com>
2025-07-09 12:42:44 -0400

    Fixed up pages repo

--- src/BrowserAutomationMaster/AppManager/AppInfo.cs
+++
@@ -1,6 +0,0 @@
-public class AppInfo
-{
-    public required string Name { get; set; } // Added required since an app won't be added if we don't know its common name
-    public string? Version { get; set; }
-    public string? Publisher { get; set; }
-}
